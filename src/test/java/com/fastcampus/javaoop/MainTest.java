3c01d0e9dfc2b437016e96adc300a34e09906b1a
Cheols <leeic@crea-m.com>
2024-12-18 12:22:51 +0900

    Implement main entity point
    실행될 메인 로직 작성하고 정렬 알고리즘 사용
    테스트는 실행 결과만 관측하고 있음. 검증 코드는 없음

---
+++ src/test/java/com/fastcampus/javaoop/MainTest.java
@@ -0,0 +1,18 @@
+package com.fastcampus.javaoop;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @Test
+  void main() {
+    // Given
+    String[] args = {"3", "1", "2"};
+
+    // When & Then
+    Main.main(args);
+  }
+
+}
